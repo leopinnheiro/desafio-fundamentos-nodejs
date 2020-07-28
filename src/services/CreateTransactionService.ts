76c73978aeb491d0e21b90fd6a3f38c2f845abe1
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2020-07-28 07:26:19 -0300

    Inclusao de funcionalidades para atender aos testes propostos.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // Se for um outcome, faz a validação de saldo.
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+      if (value > total) {
+        throw Error('You do not enough have balance.');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
